357d4afe30887d7cb4f931ce9c14ce397c212ec9
angel <angelrinascita@gmail.com>
2019-08-31 17:19:47 +0400

    bild

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -5,5 +5,4 @@ import { ModelValidatorSys } from "./Validator/ModelValidatorSys";
 import { ModelOneRuleC } from "./Validator/ModelOneRuleC";
 import { ModelRulesC, ModelRulesT } from "./Validator/ModelRulesC";
 import { BaseClass } from "./BaseClass";
-import { BaseCoreI, initBaseCore, BaseConfI } from "./BaseCore/BaseCore";
-export { ErrorSys, SimpleI, Validator, ModelValidatorSys, ModelOneRuleC, ModelRulesC, ModelRulesT, BaseClass, BaseCoreI, initBaseCore, BaseConfI };
+export { ErrorSys, SimpleI, Validator, ModelValidatorSys, ModelOneRuleC, ModelRulesC, ModelRulesT, BaseClass };
