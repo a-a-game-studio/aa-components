5a5d39e5ad9f39494685d18d1ca13476ef0a9b06
angel <angelrinascita@gmail.com>
2021-03-11 09:59:44 +0400

    remove

--- lib/BaseClass.d.ts
+++
@@ -1,8 +0,0 @@
-import { ModelValidatorSys } from './ModelValidatorSys/ModelValidatorSys';
-import { ErrorSys } from './ErrorSys/ErrorSys';
-export declare class BaseClass {
-    errorSys: ErrorSys;
-    modelValidatorSys: ModelValidatorSys;
-    constructor(errorSys: ErrorSys);
-    protected className(): string;
-}
