9a7d423e2ad4dd5f73b217014d07090b8ec43cf4
Alexander Polozov <polozov@alkor.co.ru>
2022-05-28 22:30:45 +0400

    Добавил в тест errorSys

--- tests/test_custom_valudator.ts
+++ tests/test_custom_valudator.ts
@@ -1,35 +1,39 @@
 import * as System from '../src';
 
 function valid(data: any) {
-	const rules = new System.ModelRulesC();
+  const rules = new System.ModelRulesC();
 
 
-    const fCustomRule: System.TCustomFn = (data: any): boolean =>{ 
-      console.log('fCustomRule',data);
-      return data === 22;
+  const fCustomRule: System.TCustomFn = (data: any, errorSys: System.ErrorSys): boolean => {
+    let bOk = data === 22
+    if (!bOk) {
+      errorSys.error(`valid_custom_rule`, `Мое сообшение об ошибке валидации`);
     }
+    return bOk;
+  }
 
-    rules.set(
-      rules.rule('custom_field')
+  rules.set(
+    rules.rule('custom_field')
       .require()
       .custom(fCustomRule)
-      .errorEx('custom_field', 'custom_field') 
-    )
+      .errorEx('custom_field', 'custom_field')
+  )
 
-	const validator = new System.ModelValidatorSys(new System.ErrorSys('dev'));
-	validator.fValid(rules.get(), data);
+  const validator = new System.ModelValidatorSys(new System.ErrorSys('dev'));
+  validator.fValid(rules.get(), data);
+  console.log(validator.errorSys.getErrors());
 
-	return validator.getResult();
+  return validator.getResult();
 }
 
 const resultInvalid = valid({
-	id_require_true_1: 45,
-    custom_field: 2,
+  id_require_true_1: 45,
+  custom_field: 2,
 });
 
 const resultValid = valid({
-	id_require_true_1: 45,
-    custom_field: 22,
+  id_require_true_1: 45,
+  custom_field: 22,
 });
 
 console.log('======================================');
